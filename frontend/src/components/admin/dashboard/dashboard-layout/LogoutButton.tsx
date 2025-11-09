1f8346d3634388bfe238af72779a4c70116b6f26
Zoli06 <csaszar.zoltan24@gmail.com>
2025-11-09 03:24:51 +0100

    logout

--- frontend/src/components/admin/dashboard/dashboard-layout/LogoutButton.tsx
+++ frontend/src/components/admin/dashboard/dashboard-layout/LogoutButton.tsx
@@ -1,13 +1,15 @@
 ﻿import { Button } from "react-daisyui";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth.ts";
 
 export const LogoutButton = () => {
   const navigate = useNavigate();
+  const { logout } = useAuth();
 
   return (
     <Button
       onClick={() => {
-        localStorage.setItem("token", "");
+        logout();
         navigate("/admin");
       }}
       color="error"
