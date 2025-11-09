768e98baae7dac5d740433ec7cd1574f0fe97aca
Zoli06 <csaszar.zoltan24@gmail.com>
2025-11-09 22:32:51 +0100

    Fix min width

--- frontend/src/components/base-layout/BaseLayout.tsx
+++ frontend/src/components/base-layout/BaseLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export const BaseLayout = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <Artboard className="max-w-4xl relative p-4 w-fit">
+      <Artboard className="max-w-4xl relative p-4 w-fit min-w-[580px]">
         <Outlet />
       </Artboard>
     </div>
