1336e9c88779b8f487007598ea7201abb1f0f764
Zoli06 <csaszar.zoltan24@gmail.com>
2025-11-09 20:14:28 +0100

    Layout center fix

--- frontend/src/components/base-layout/BaseLayout.tsx
+++ frontend/src/components/base-layout/BaseLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export const BaseLayout = () => {
   return (
-    <div className="flex justify-center items-start min-h-screen pt-[30vh]">
+    <div className="flex justify-center items-center min-h-screen">
       <Artboard className="max-w-3xl relative p-4">
         <Outlet />
       </Artboard>
