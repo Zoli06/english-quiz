fcdf4790c5c525010dbb583d34d57b817f8c8be7
Zoli06 <csaszar.zoltan24@gmail.com>
2025-11-09 21:17:57 +0100

    build fix

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 cd frontend
+npm install
 npm run build
+cd ..
+cd backend
+npm install
 cd ..
 rm -rf backend/public
 cp -r frontend/dist backend/public
-docker compose build
+sudo docker compose build
 # to run the containers, use: docker compose up -d
